fc8dd469c73da540fb092a62535ba6c37ba6c1bb
Waqas Saeed <w.saeed77@gmail.com>
2025-10-13 10:40:52 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -89,7 +89,7 @@ Route::prefix('eco4')->middleware('auth')->group(function () {
 
 // Settings routes (admin only)
 use App\Http\Controllers\SettingsController;
-Route::middleware(['auth', 'permission:setting.manage'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
     Route::put('/settings/{setting}', [SettingsController::class, 'update'])->name('settings.update');
     Route::post('/settings/api/update', [SettingsController::class, 'apiUpdate'])->name('settings.api.update');
